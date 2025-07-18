1a7890d8c6648385a58e4ef4f35f68212a71b42b
shivamo7s <shivam@o7tec.com>
2025-07-18 23:03:00 +0530

    first commit

---
+++ 01_create-instence.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my-server-01" {
+  ami           = "ami-0a1235697f4afa8a4"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "my-tag"
+  }
+}
+
