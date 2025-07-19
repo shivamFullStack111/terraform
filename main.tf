e08a7ca4bd9ebed349fda57aaca914fe9a3f7a98
shivamo7s <shivam@o7tec.com>
2025-07-19 12:38:53 +0530

    random provider

--- main.tf
+++ main.tf
@@ -6,20 +6,35 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.4.0"
     }
+
+    random = { // for random provider
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
 
-
 provider "aws" {
-  region = "ap-south-1" // use custom variable 
+  region = "ap-south-1"
 }
 
+# random id generate 
+resource "random_id" "randomId" {
+  byte_length = 4
+}
+
+# test the random id by print 
+output "random-id-output" {
+  value = random_id.randomId.dec
+}
+
+
 resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-bucket409854"
+  bucket = "my-bucket-${random_id.randomId.b64_url}" // use random id to avoid duplicate s3 bucket
 }
 
 resource "aws_s3_object" "my-object" {
   bucket = aws_s3_bucket.my-bucket.bucket
   source = "./myTextfile"
-  key = "myfile"
+  key    = "myfile"
 }
