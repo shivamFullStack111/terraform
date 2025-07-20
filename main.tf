2811d024e10d51b5a08f0310a2a5ff4d5ac4655f
shivamo7s <shivam@o7tec.com>
2025-07-20 14:02:20 +0530

    how to crate vpc,subnet,route table, internet gateway and create connections NOTE: must use docs of terraform

--- main.tf
+++ main.tf
@@ -4,13 +4,12 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.4.0"
     }
+  
   }
-
-  # this manage terraform state .tfstate file in s3 bucket 
   backend "s3" {
-    bucket = "my-buckert-483943"
-    key    = "terraform.tfstate"
     region = "ap-south-1"
+    bucket = "my-bucket-987789"
+    key    = "terraform.tfstate"
   }
 }
 
@@ -18,67 +17,58 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# create s3 bucket 
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-buckert-for-static-hosting-5667"
-}
-
-# upload index.html file to s3
-resource "aws_s3_bucket_object" "index-html-file" {
-  bucket       = aws_s3_bucket.my-bucket.bucket
-  source       = "./index.html"
-  key          = "index.html"
-  content_type = "text/html" // this help to known s3 that file is html type and render correctly in static website hosting
-
-}
-# upload index.css file to s3 
-resource "aws_s3_bucket_object" "index-css-file" {
-  key          = "index.css"
-  bucket       = aws_s3_bucket.my-bucket.bucket
-  source       = "./index.css"
-  content_type = "text/css" // this help to correctly apply css to index.html 
-
-}
-
-# change policy of s3 bucket to access static website
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.my-bucket.id
-  # jsondecode convert hcl to json and change polity in s3 bucket 
-  policy = jsonencode(
-    {
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Sid       = "Statement1"
-          Effect    = "Allow"
-          Principal = "*"
-          Action    = "s3:GetObject"
-          Resource  = "arn:aws:s3:::${aws_s3_bucket.my-bucket.id}/*"
-        }
-      ]
-    }
-  )
-}
-
-# enable public access for static website hosting
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.my-bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
 
 
-# this block enable static website hosting and provide index.html as root for access 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.my-bucket.id
-  index_document {
-    suffix = "index.html"
+# create vpc 
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
   }
 }
 
-# this print endpoint/url of static website hosting
-output "print-static-website-endpoint" {
-  value = aws_s3_bucket_website_configuration.example.website_endpoint
+# create private subnet 
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "private-subnet"
+  }
 }
+
+# create public subnet 
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# create internet gateway 
+resource "aws_internet_gateway" "my-internet-gateway" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-internet-gateway"
+  }
+}
+
+# create route table 
+resource "aws_route_table" "my-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-internet-gateway.id
+  }
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+# route table association  connect route table with public subnet and internet gateway
+resource "aws_route_table_association" "my-route-table-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.my-route-table.id
+}
+
+
