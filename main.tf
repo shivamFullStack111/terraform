2ebe4d28d5a01f09cc2639ab043aafb41f4a7536
shivamo7s <shivam@o7tec.com>
2025-07-21 20:08:47 +0530

    loops , operators

--- main.tf
+++ main.tf
@@ -12,12 +12,43 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# this is local values block in this block we can declare a variables. 
-# in this variables we cannot provide values outside the file.
-locals {
-  instance_type = "t2.nano"
+variable "list_0f_num" {
+  type    = list(number)
+  default = [0, 1, 2, 3, 4, 5]
 }
 
-output "instance-type" {
-  value = local.instance_type
+variable "person" {
+  type = object({
+    name  = string
+    class = string
+  })
+  default = {
+    name  = "shivam"
+    class = "+2 Arts"
+  }
+}
+
+
+variable "map_var" {
+  type = map(number)
+  default = {
+    ram   = 20
+    sham  = 40
+    karan = 38
+  }
+}
+
+
+locals {
+  list_of_num_double = [for num in var.list_0f_num : num * 2]
+  # this loop again create map by doubling the current value 
+  map_var_double = { for key, value in var.map_var : key => value * 2 }
+}
+
+
+output "list_of_num_double" {
+  value = local.list_of_num_double
+}
+output "map_var_double" {
+  value = local.map_var_double
 }
