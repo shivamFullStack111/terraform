0ce4e3105374b6041e8c6c5ef22383315fab79f2
shivamo7s <shivam@o7tec.com>
2025-07-21 16:52:38 +0530

    local values

--- main.tf
+++ main.tf
@@ -6,68 +6,18 @@ terraform {
     }
 
   }
-  backend "s3" {
-    region = "ap-south-1"
-    bucket = "my-bucket-987789"
-    key    = "terraform.tfstate"
-  }
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
 
-# this is simple variable
-variable "aws_bucket" {
-  type    = string
-  default = "my-bucket-049438"
+# this is local values block in this block we can declare a variables. 
+# in this variables we cannot provide values outside the file.
+locals {
+  instance_type = "t2.nano"
 }
 
-# default value is not provided this will ask you value in terminal with their description as a message 
-# or we can provide value in terminal using ENV by writting =>     $env:TF_VAR_aws_ami = "ami-1234567890abcdef" (Windows) ,  export TF_VAR_aws_ami=ami-833773374 (Linux,macOS)  
-variable "aws_ami" {
-  type        = string
-  description = "Enter aws ami for instance"
+output "instance-type" {
+  value = local.instance_type
 }
-
-# when we have to store multiple data in same varibale then use object ACCESS: var.aws_block.name
-variable "aws_block" {
-  type = object({
-    name  = string
-    class = string
-  })
-
-  default = {
-    name  = "Shivam"
-    class = "+2 Arts"
-  }
-}
-
-# map which is key value pair 
-variable "map_variable" {
-  type = map(string)
-  default = {
-    "this_is_key" = "this_is_value"
-  }
-}
-
-
-
-
-
-# outputs-----------------------------------------------------------------------
-
-output "aws_bucket" {
-  value = var.aws_bucket
-}
-output "ami" {
-  value = var.aws_ami
-}
-output "aws_block" {
-  value = "studnet details => Name: ${var.aws_block.name}, Class: ${var.aws_block.class}"
-}
-output "map_variable" {
-  value = var.map_variable
-}
-
-# ------------------------------------------------------------------------------
