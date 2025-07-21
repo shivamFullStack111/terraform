256a6c6d266461724f9bdbcf9eb985ff72b0534d
shivamo7s <shivam@o7tec.com>
2025-07-21 23:23:53 +0530

    count => which is used to create multiple resources in single code

--- main.tf
+++ main.tf
@@ -12,43 +12,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-variable "list_0f_num" {
-  type    = list(number)
-  default = [0, 1, 2, 3, 4, 5]
-}
-
-variable "person" {
-  type = object({
-    name  = string
-    class = string
-  })
-  default = {
-    name  = "shivam"
-    class = "+2 Arts"
+resource "aws_instance" "my-instance" {
+  ami           = "ami-0a1235697f4afa8a4"
+  instance_type = "t2.micro"
+  count = 2 // this line count = 2 create 2 instance means this line run this block 2 times
+  tags = {
+    Name = "my-instance-${count.index}" // count.index is index which starts from 0 to n-1 
   }
-}
-
-
-variable "map_var" {
-  type = map(number)
-  default = {
-    ram   = 20
-    sham  = 40
-    karan = 38
-  }
-}
-
-
-locals {
-  list_of_num_double = [for num in var.list_0f_num : num * 2]
-  # this loop again create map by doubling the current value 
-  map_var_double = { for key, value in var.map_var : key => value * 2 }
-}
-
-
-output "list_of_num_double" {
-  value = local.list_of_num_double
-}
-output "map_var_double" {
-  value = local.map_var_double
 }
