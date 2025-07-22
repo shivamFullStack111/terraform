5f006492363e3fb7e6753542f5e36d6926b64fc8
shivamo7s <shivam@o7tec.com>
2025-07-22 11:42:29 +0530

    for each : create instances by using for_each in map and also find index of key in every itteration

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.4.0"
     }
-
   }
 }
 
@@ -12,11 +11,30 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+variable "ami_map" {
+  type = map(object({
+    ami           = string
+    instance_type = string
+  }))
+
+  default = {
+    "amazon" = {
+      ami           = "ami-0b32d400456908bf9"
+      instance_type = "t2.nano"
+    }
+    "ubuntu" = {
+      ami           = "ami-0f918f7e67a3323f0"
+      instance_type = "t2.nano"
+    }
+  }
+}
+
+
 resource "aws_instance" "my-instance" {
-  ami           = "ami-0a1235697f4afa8a4"
-  instance_type = "t2.micro"
-  count = 2 // this line count = 2 create 2 instance means this line run this block 2 times
+  for_each      = var.ami_map // for each work with only set or map
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
   tags = {
-    Name = "my-instance-${count.index}" // count.index is index which starts from 0 to n-1 
+    "Name" = "my-instance-${index(keys(var.ami_map), each.key)}-ami=>${each.key}" // in this we find index of current iteration key keys() get all keys from map and index find(map,single_key) this find current key index from map
   }
 }
