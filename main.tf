7cd2fce8c572e332c1ca8d3cdf0ab1a4ca2a9331
shivamo7s <shivam@o7tec.com>
2025-07-23 21:45:20 +0530

    Create dynamic and reusable VPC module with conditional public subnets, Internet Gateway, route table, and subnet associations

--- main.tf
+++ main.tf
@@ -1,34 +1,40 @@
-terraform {
-  required_providers {
-    name = {
-      source  = "hashicorp/aws"
-      version = "6.4.0"
-    }
-  }
-}
-
+# ----------------------------------------
+# AWS Provider Block
+# ----------------------------------------
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1"  # 👈 This sets the AWS region where all resources will be deployed (Mumbai region)
 }
 
+# ----------------------------------------
+# VPC Module Block
+# ----------------------------------------
+module "vpc_module" {
+  source = "./module/vpc"  # 👈 Refers to a local Terraform module located in 'module/vpc' directory
 
-variable "my_var" {
-  type    = bool
-  default = true
-}
+  # -------------------------------
+  # List of Subnets to be created
+  # -------------------------------
+  subnet_config = [
+    {
+      availability_zone = "ap-south-1a",     # 👈 First subnet in AZ 'a'
+      cidr_block        = "10.0.0.0/24"      # 👈 256 IPs in this range
+    },
+    {
+      availability_zone = "ap-south-1b",     # 👈 Second subnet in AZ 'b'
+      cidr_block        = "10.0.1.0/24"
+    },
+    {
+      availability_zone = "ap-south-1c",     # 👈 Third subnet in AZ 'c'
+      cidr_block        = "10.0.2.0/24",
+      is_public         = true              # ✅ This subnet is marked public. Useful for EC2s that need internet
+    },
+  ]
 
-resource "aws_instance" "my-instance" {
-  # In this line, if the value of `my_var` is true, then count will be set to 1,
-  # which means the resource block will execute once and one instance will be created.
-  # If the value of `my_var` is false, then count will be set to 0,
-  # which means this resource block will not run and no instance will be created.
-
-  count = var.my_var == true ? 1 : 0
-
-  ami           = "ami-0b32d400456908bf9"
-  instance_type = "t2.nano"
-  tags = {
-    Name = "my-instance"
+  # -------------------------------
+  # VPC Configuration
+  # -------------------------------
+  vpc_config = {
+    cidr_block = "10.0.0.0/16",   # 👈 Full VPC has 65536 IPs (large private IP block)
+    name       = "vpc-test"       # 👈 Name tag used in resources for identification
   }
 }
-
